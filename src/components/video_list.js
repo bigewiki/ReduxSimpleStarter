387b3637f5e2bc0365cf77b2fc070960da322451
Edward M <edwardtmuniz@gmail.com>
2018-06-25 20:59:51 -0700

    using map to loop through array items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item.js';
 
 const VideoList = (props) => {
+  const VideoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  })
+
   return(
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {VideoItems}
     </ul>
   );
 };
