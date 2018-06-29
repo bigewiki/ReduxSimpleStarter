45a2ba43da952bef511e5bc9122096364031819d
Edward M <edwardtmuniz@gmail.com>
2018-06-28 20:39:00 -0700

    added markup and displaying video thumbnails and titles

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  const imageURL = video.snippet.thumbnails.default.url;
+
   return(
-    <li>Video</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+
+    </li>
   );
 };
 
