2111696123c7084899eaa509a7f99e7630e2e0db
Edward M <edwardtmuniz@gmail.com>
2018-06-28 20:17:45 -0700

    added key as video list property

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,10 @@ import VideoListItem from './video_list_item.js';
 
 const VideoList = (props) => {
   const VideoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem
+    key={video.etag}
+    video={video}
+  />
   })
 
   return(
