c28c0446bcd5e15b41f95a8ccf2ce6077dc418e7
Edward M <edwardtmuniz@gmail.com>
2018-06-29 21:28:13 -0700

    working onclick video update, onVideoSelect function passed through props

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,6 +4,7 @@ import VideoListItem from './video_list_item.js';
 const VideoList = (props) => {
   const VideoItems = props.videos.map((video) => {
     return <VideoListItem
+    onVideoSelect={props.onVideoSelect}
     key={video.etag}
     video={video}
   />
