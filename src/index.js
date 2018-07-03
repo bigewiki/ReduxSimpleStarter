2c3013b50755188375ba8c0b027bac43c8998c82
Edward M <edwardtmuniz@gmail.com>
2018-07-02 20:59:46 -0700

    added lodash to limit updates to 3 seconds after typing stops

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail';
@@ -26,9 +27,11 @@ class App extends Component{
   }
 
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) },300 )
+
     return(
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
